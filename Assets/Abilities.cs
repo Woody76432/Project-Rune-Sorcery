28c388dcc09da87a01c32815217aba0cf835ff3b
Woody76432 <woody7652@gmail.com>
2024-10-04 13:17:39 +0100

    Added ability class functions
    ability cooldowns can now be manipulated by other scripts and also checked by other scripts.

--- Assets/Abilities.cs
+++ Assets/Abilities.cs
@@ -8,15 +8,50 @@ public class Abilities : MonoBehaviour
     public static float ability1Cooldown = -75f;
     public static float ability2Cooldown = -75f;
     public static float ability3Cooldown = -75f;
+
+    public static float ability1CooldownMultiplier = 10;
+    public static float ability2CooldownMultiplier = 2;
+    public static float ability3CooldownMultiplier = 3;
+
+    // Setters for other scripts to influence cooldown timers -----------------------------------------------------------------------------------------------------------------
+    public static void SetAbility1CooldownMultiplier(int multiplier)
+    {
+        ability1CooldownMultiplier = multiplier;
+    }
    
+    public static void SetAbility2CooldownMultiplier(int multiplier)
+    {
+        ability2CooldownMultiplier = multiplier;
+    }
+
+    public static void SetAbility3CooldownMultiplier(int multiplier)
+    {
+        ability3CooldownMultiplier = multiplier;
+    }
+
+
+    // Getters for other scripts to check cooldowns -----------------------------------------------------------------------------------------------------------------------
+    public static float GetAbility1Cooldown()
+    {
+        return ability1Cooldown;
+    }
+
+    public static float GetAbility2Cooldown()
+    {
+        return ability2Cooldown;
+    }
+
+    public static float GetAbility3Cooldown()
+    {
+        return ability3Cooldown;
+    }
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------
 
     public GameObject ability1Mask;
     public GameObject ability2Mask;
     public GameObject ability3Mask;
 
     // Set the cooldown to 0, subtract time.deltatime to cooldown until it reaches 75 or over then end
-
-
 
     // Start is called before the first frame update
     void Start()
@@ -53,11 +88,11 @@ public class Abilities : MonoBehaviour
         }
  
 
-        // Move the masks into its pos X of cooldown
+        // Move the masks into its posX of "cooldown" -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
         if (ability1Cooldown!= -75f)
         {
             ability1Mask.GetComponent<RectTransform>().anchoredPosition = new Vector2(0f, ability1Cooldown);
-            ability1Cooldown -= (Time.deltaTime*10);
+            ability1Cooldown -= (Time.deltaTime*ability1CooldownMultiplier);
             if (ability1Cooldown < -75f)
             {
                 ability1Cooldown = -75f;
@@ -68,7 +103,7 @@ public class Abilities : MonoBehaviour
         if (ability2Cooldown != -75f)
         {
             ability2Mask.GetComponent<RectTransform>().anchoredPosition = new Vector2(0f, ability2Cooldown);
-            ability2Cooldown -= (Time.deltaTime * 5);
+            ability2Cooldown -= (Time.deltaTime * ability2CooldownMultiplier);
             if (ability2Cooldown < -75f)
             {
                 ability2Cooldown = -75f;
@@ -78,11 +113,19 @@ public class Abilities : MonoBehaviour
         if (ability3Cooldown != -75f)
         {
             ability3Mask.GetComponent<RectTransform>().anchoredPosition = new Vector2(0f, ability3Cooldown);
-            ability3Cooldown -= (Time.deltaTime * 5);
+            ability3Cooldown -= (Time.deltaTime * ability3CooldownMultiplier);
             if (ability3Cooldown < -75f)
             {
                 ability3Cooldown = -75f;
             }
+        }
+
+        //Testing
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            SetAbility1CooldownMultiplier(200);
+            SetAbility2CooldownMultiplier(40);
+            SetAbility3CooldownMultiplier(47);
         }
 
     }
