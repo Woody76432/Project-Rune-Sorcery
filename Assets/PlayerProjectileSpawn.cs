adcc106454bee44667d481b88fa248e212f8ec5b
Woody76432 <woody7652@gmail.com>
2024-11-07 15:00:35 +0000

    higuhui

--- Assets/PlayerProjectileSpawn.cs
+++ Assets/PlayerProjectileSpawn.cs
@@ -11,7 +11,7 @@ public class ProSpawn : MonoBehaviour
     public GameObject arrowSpawnPosition2;
     public GameObject arrowSpawnPosition3;
 
-    public static string weaponName = "null";
+    public static string weaponName = "lasers";
 
     public static string SetWeaponString(string newWeaponString)
     {
@@ -22,21 +22,28 @@ public class ProSpawn : MonoBehaviour
 
     void Update()
     {
-        // Determine what kind of weapon the player should fire based on a string that can be updated by the weapon through a function
-        switch (weaponName)
+        if (Input.GetKeyDown(KeyCode.Mouse0)==true)
         {
-            case "null":
-                Debug.Log("Player has no weapon so cant spawn a projectile");
-                break;
-            case "arrows":
-                // Instantiate 3 different arrows at the different projectile spawn positions
-                break;
-            case "lasers":
-                // Instantiate 1 laser at projectile spawn location
-                break;
-            case "rockets":
-                //Instantiate 1 rocket projectile that can go through multiple enemies and flys erratically before deleting itself
-                break;
+            switch (weaponName)
+            {
+                case "null":
+                    Debug.Log("Player has no weapon so cant spawn a projectile");
+                    break;
+                case "arrows":
+                    // Instantiate 3 different arrows at the different projectile spawn positions
+                    Instantiate(arrows, projectileSpawnLocation.transform.position, projectileSpawnLocation.transform.rotation);
+                    break;
+                case "lasers":
+                    // Instantiate 1 laser at projectile spawn location
+                    Instantiate(lasers, projectileSpawnLocation.transform.position, projectileSpawnLocation.transform.rotation);
+                    break;
+                case "rockets":
+                    //Instantiate 1 rocket projectile that can go through multiple enemies and flys erratically before deleting itself
+                    Instantiate(rockets, projectileSpawnLocation.transform.position, projectileSpawnLocation.transform.rotation);
+                    break;
+            }
         }
+        // Determine what kind of weapon the player should fire based on a string that can be updated by the weapon through a function
+
     }
 }
