1c70e00c0f24e49b0f3b060ec95d098012d52ab9
Woody76432 <woody7652@gmail.com>
2024-11-07 08:34:02 +0000

    Started individual weapons

--- Assets/PlayerProjectileSpawn.cs
+++ Assets/PlayerProjectileSpawn.cs
@@ -4,30 +4,39 @@ using UnityEngine;
 
 public class ProSpawn : MonoBehaviour
 {
-    public GameObject projectile;
-    public GameObject grenade;
+    public GameObject arrows;
+    public GameObject lasers;
+    public GameObject rockets;
     public GameObject projectileSpawnLocation;
+    public GameObject arrowSpawnPosition2;
+    public GameObject arrowSpawnPosition3;
 
+    public static string weaponName = "null";
 
-    // Start is called before the first frame update
-    void Start()
+    public static string SetWeaponString(string newWeaponString)
     {
-        
+        weaponName = newWeaponString;  
+        // Also returns the weapon name incase I need it later for anything
+        return weaponName;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0)) 
+        // Determine what kind of weapon the player should fire based on a string that can be updated by the weapon through a function
+        switch (weaponName)
         {
-            Instantiate(projectile,projectileSpawnLocation.transform.position,projectileSpawnLocation.transform.rotation);
-        }
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            if (Abilities.ability1Cooldown == -75f)
-            {
-                Instantiate(grenade, projectileSpawnLocation.transform.position, projectileSpawnLocation.transform.rotation);
-            }
+            case "null":
+                Debug.Log("Player has no weapon so cant spawn a projectile");
+                break;
+            case "arrows":
+                // Instantiate 3 different arrows at the different projectile spawn positions
+                break;
+            case "lasers":
+                // Instantiate 1 laser at projectile spawn location
+                break;
+            case "rockets":
+                //Instantiate 1 rocket projectile that can go through multiple enemies and flys erratically before deleting itself
+                break;
         }
     }
 }
