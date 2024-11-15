bd37a6c07e598f6574b85eb975a6eec8c34ad141
Woody76432 <woody7652@gmail.com>
2024-11-15 15:54:58 +0000

    Basic PinkOne AI
    Still need to add the ranged AI, the minions spawning. and also needs to add damage on impact with certain projectiles, I could make the scripts for the projectiles really long and do different things dependent on the tag.

--- Assets/PinkOneAI.cs
+++ Assets/PinkOneAI.cs
@@ -5,43 +5,65 @@ using UnityEngine;
 public class PinkOneAI : MonoBehaviour
 {
     public GameObject player;
-    public float moveSpeed;
+    public GameObject pinkOne;
+    public float moveSpeed = 1;
     public float distance;
-    public int pinkOneHealth = 1000;
-    public gameObject minionSpawnArea;
-    public gameObject projectileSpawnArea;
-    public gameObject projectile;
+    public GameObject minionSpawnArea;
+    public GameObject projectileSpawnArea;
+    public GameObject projectile;
     public float fireRate;
     public int phase = 0;
+    public static int staticPinkOneHealth=1000;
+    public int pinkOneHealth=1000;
+
+    void Start()
+    {
+        moveSpeed = 2;
+    }
 
     public static void PinkOneDamage(int damage)
     {
-        pinkOneHealth-=damage;
+        staticPinkOneHealth-=damage;
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        if (pinkOneHealth>666)
+
+        pinkOneHealth = staticPinkOneHealth;
+        if (pinkOneHealth>=666)
         {
             //Moves the entity this script is on towards the Player
             distance = Vector2.Distance(transform.position, player.transform.position);
             Vector2 direction = player.transform.position - transform.position;
-            if (distance < 10)
+
+            if (distance < 5)
             {
                 transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, moveSpeed * Time.deltaTime);
             }
+
         }
         if (pinkOneHealth > 333 && pinkOneHealth < 666)
         {
             // Wizard type AI, move away and shoot
+            moveSpeed = 1;
+            distance = Vector2.Distance(transform.position, player.transform.position);
+            Vector2 direction = player.transform.position - transform.position;
+
+            if (distance>0&&distance<10)
+            {
+                transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, -moveSpeed * Time.deltaTime);
+            }
+
+            // On collision with a projectile tag, fetch the projectile.damageInt 
+
         }
         if (pinkOneHealth < 333 && pinkOneHealth > 0)
         {
             // Also spawn minions
         }
-        if (pinkOneHealth=<0)
+        if (pinkOneHealth <= 0)   
         {
 
         }
