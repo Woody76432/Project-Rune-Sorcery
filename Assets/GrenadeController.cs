c3c01dcdbf24b54aecfea81f4b6c95c930ea49a2
Woody76432 <woody7652@gmail.com>
2024-11-27 12:51:26 +0000

    Began Turret, 90% grenade
    Just need to make the grenade do damage on impact and detonation. Added turret sprites

--- Assets/GrenadeController.cs
+++ Assets/GrenadeController.cs
@@ -5,16 +5,39 @@ using UnityEngine;
 public class GrenadeController : MonoBehaviour
 {
     public Rigidbody2D grenade;
+    public float grenadeFuse = 2;
+    public GameObject explosionEffect;
 
     // Start is called before the first frame update
     void Start()
     {
-        grenade = GetComponent<Rigidbody2D>();    
+        grenade = GetComponent<Rigidbody2D>();
+        grenade.velocity = new Vector3(0, 10, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-       // grenade.velocity=Vector2(10,0,0);
+        grenadeFuse-=Time.deltaTime;
+        if (grenadeFuse <= 0)
+        {
+            Detonate();
+            if (grenadeFuse<=-0.5)
+            {
+               
+                Destroy(gameObject);
+                Instantiate(explosionEffect,transform.position,transform.rotation); 
+            }
+        }
+    }
+    private void Detonate()
+    {
+        void OnTriggerEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag=="enemy")
+            {
+                Destroy(collision.gameObject);
+            }
+        }
     }
 }
